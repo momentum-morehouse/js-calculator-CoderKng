3258866e56c9963a5ed4be1e7e4418c24d9ae9e8
Tyrone Wade <gcmc_tyronewade@yahoo.com>
2020-09-25 06:17:03 -0700

    first commit calculator

---
+++ script.js
@@ -0,0 +1,21 @@
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement){ 
+        this.previousOperandTextElement = previousOperandTextElement 
+        this.currentOerandTextElement = currentOperandTextElement
+    }
+}
+}
+
+
+
+
+
+
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = ducoment.querySelrctor('[data-operation]')
+const equalsButtons = document.querySelectorAll('[data-equals]')
+const deleteButtons = document.querySelectorAll('[data-delete]')
+const allClearrButtons = document.querySelectorAll('[data-number-previous-opreand]')
+const previousOperanButtons = document.querySelectorAll('[data-number]')
+const currentButtons = document.querySelectorAll('[data-number]')
